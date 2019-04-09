9756168f2176262f3faa256ebde91308ebcbca07
m.michurov <m.michurov@g.nsu.ru>
2019-04-09 12:10:26 +0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ArithmeticCoding.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ArithmeticCoding.dir/main.c.obj"
+  "ArithmeticCoding.pdb"
+  "ArithmeticCoding.exe"
+  "ArithmeticCoding.exe.manifest"
+  "libArithmeticCoding.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ArithmeticCoding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ArithmeticCoding.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ArithmeticCoding.
+# This may be replaced when dependencies are built.

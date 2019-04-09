51c109a09b12debc7af685fa3acf06c0dfab9467
m.michurov <m.michurov@g.nsu.ru>
2019-04-09 12:25:23 +0700

    owo

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
 
 
-    
+    double a = 7.0;
+
+    unsigned long long * p;
+
+    for (int y = 0; y < 25; y++) {
+        a /= 2;
+        p = (unsigned long long *) &a;
+        printf("\n%.20lf\t", a);
+
+        for (unsigned int k = 8 * sizeof(double) - 1; k >= 0; k--) {
+            printf("%llu", ((*p) >> k) & 0x01);
+            if ((k) % 8 == 0)
+                printf(" ");
+        }
+    }
 
 
     return 0;
